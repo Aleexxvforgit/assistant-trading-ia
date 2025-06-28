f696e10eb4a63ca7132480caa1afda1246d52417
Alexandre VEYLET <alexandre.veylet.pro@gmail.com>
2025-06-28 18:19:20 +0200

    test message commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,13 @@ export default {
     }
   },
   runtimeConfig: {
-    MISTRAL_API_KEY: process.env.MISTRAL_API_KEY
+    MISTRAL_API_KEY: process.env.MISTRAL_API_KEY,
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
   },
   nitro: {
     compatibility: {
@@ -16,5 +22,6 @@ export default {
   },
   typescript: {
     strict: true
-  }
+  },
+  plugins: ['~/plugins/firebase.ts']
 }
